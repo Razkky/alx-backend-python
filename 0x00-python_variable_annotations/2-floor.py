1d3a2447971b748e75666750bd34dca1705cfb96
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 12:30:23 +0100

    return the floor of a floating point

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""This script returns to floor of a floating point number"""
+import math
+
+def floor(n: float) -> int:
+    """Returns the floor of a floating point"""
+
+    return math.floor(n)
