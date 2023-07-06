c1844ed9ce42d18c5453306035034770cfdbf2b3
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 12:00:13 +0100

    concatinate two strings

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script concatinate two strings together"""
+
+def concat(str1: str, str2: str) -> str:
+    """Accepts two strings and return the concatinated string"""
+
+    return str1 + str2
