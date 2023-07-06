2618433e6bdf8ff1957374389291599a6dba025a
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 16:09:36 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/6-sum_mixed_list.py
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -1,7 +1,10 @@
 #!/usr/bin/env python3
 """This scripts takes a list of flaot and it and return thier sum as float"""
+from typing import List
 
-mixed_list = list[float, int]
+mixed_list = List[float, int]
+
+
 def sum_mixed_list(mxd_lst: mixed_list) -> float:
     """Returns the sum of a mixed list as float"""
     sum = 0
