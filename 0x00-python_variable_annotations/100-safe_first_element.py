6b027dc3ea2236c60d33466fc39a40224819fb24
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 16:35:10 +0100

    annotate function

---
+++ 0x00-python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This script annotate the fucntion below"""
+from typing import Sequence, Any, Optional, Union
+
+def safe_first_element(lst: Sequence[Any]) -> Union[Any, Optional[None]]:
+    """Annotate function"""
+    if lst:
+        return lst[o]
+    else:
+        return None
