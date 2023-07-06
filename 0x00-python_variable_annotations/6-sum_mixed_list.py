8575f063f1abc307d8fb8bcb6f960b168bb3ff16
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 15:36:56 +0100

    add mixed numbers in a list

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""This scripts takes a list of flaot and it and return thier sum as float"""
+
+mixed_list = list[float, int]
+def sum_mixed_list(mxd_lst: mixed_list) -> float:
+    """Returns the sum of a mixed list as float"""
+    sum = 0
+    for num in mxd_lst:
+        sum += num
+    return sum
