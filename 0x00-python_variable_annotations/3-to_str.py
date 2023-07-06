fa18e75930612d8a9c763170d87fbb20a9f612ee
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 12:30:39 +0100

    return str of a floating number

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""This script takes a floating number and return its string repr"""
+
+def to_str(n: float) -> str:
+    """Returns the string representation of a flaoting point number"""
+
+    return str(n)
