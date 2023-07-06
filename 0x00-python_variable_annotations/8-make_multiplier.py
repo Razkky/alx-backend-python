26077089cdc51b03dc425dfc7cbe3c9bd34845f8
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 16:11:41 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/8-make_multiplier.py
+++ 0x00-python_variable_annotations/8-make_multiplier.py
@@ -2,6 +2,7 @@
 """This script returns a function that multiplies a float by a multiplier"""
 from typing import Callable
 
+
 def make_multiplier(multiplier: float) -> Callable[[float], float]:
     """Returns a callable funciton"""
     def multiply(number: float) -> float:
