13651fe21273f1a5ea7e449d9c0456f7694bd87d
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 11:48:33 +0100

    add two floating number

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""An annotation function that adds two float and returns an float"""
+
+
+def add(a: float, b: float) -> float:
+    """This function add two floating number
+        a: float
+        b: flaot
+    """
+    return a + b
