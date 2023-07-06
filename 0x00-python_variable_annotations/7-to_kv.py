737a62c4642e4b9db1ad7c089abb7aaf9d467d07
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 16:11:07 +0100

    fix pycodestyle

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -2,6 +2,7 @@
 """This scripts takes a str and int or float and returns a tuple"""
 from typing import Union, Tuple
 
+
 def to_kv(k: str,  v: Union[int, float]) -> Tuple[str, float]:
     """Takes a string and int or float and returns a tuple"""
     variable: Tuple[str, float]
