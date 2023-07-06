ad0aa4e5c8bc1d340e67aac5032f0e0a888d0ac8
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-06 15:37:15 +0100

    return a tuple

---
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""This scripts takes a str and int or float and returns a tuple"""
+from typing import Union, Tuple
+
+def to_kv(k: str,  v: Union[int, float]) -> Tuple[str, float]:
+    """Takes a string and int or float and returns a tuple"""
+    variable: Tuple[str, float]
+    variable = (k, v * v)
+    return variable
