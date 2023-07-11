8d39839d233d309b2e8a4507da7cab4483fb18a3
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-11 06:50:10 +0100

    change funciton type

--- 0x01-python_async_function/2-measure_runtime.py
+++ 0x01-python_async_function/2-measure_runtime.py
@@ -6,12 +6,10 @@ import random
 wait_n = __import__('1-concurrent_coroutines').wait_n
 
 
-async def measure_time(n: int, max_delay: int = 10) -> float:
+def measure_time(n: int, max_delay: int = 10) -> float:
     """Measures the execution time of wait_n function"""
     execution_time: float
-
     start = time.perf_counter()
-    await wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     execution_time = time.perf_counter() - start
-    print("returning")
     return execution_time / n
