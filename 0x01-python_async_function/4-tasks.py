74cc97a8624efb192f6f31dc220d97ce24517952
Abdul Razak <muhammadjamiuabdulrazak@gmail.com>
2023-07-11 06:39:00 +0100

    remove additional argument

--- 0x01-python_async_function/4-tasks.py
+++ 0x01-python_async_function/4-tasks.py
@@ -2,11 +2,12 @@
 """This script takes an int and wait for random delay"""
 import asyncio
 import random
+from typing import List
 task_wait_random = __import__('3-tasks').task_wait_random
 
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """"Returns a random delay"""
-    tasks = [task_wait_random(task_id, max_delay) for task_id in range(n)]
+    tasks = [task_wait_random(max_delay) for task_id in range(n)]
     results = await asyncio.gather(*tasks)
     return sorted(results)
